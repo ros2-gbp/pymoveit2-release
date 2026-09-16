26550911a16ae103e4f7f53545edfd6548c3db30
Andrej Orsula <orsula.andrej@gmail.com>
2026-09-16 18:06:55 +0200

    Rebase from 'upstream'

--- pymoveit2/__init__.py
+++ pymoveit2/__init__.py
@@ -4,3 +4,19 @@ from .gripper_interface import GripperInterface
 from .moveit2 import MoveIt2, MoveIt2State
 from .moveit2_gripper import MoveIt2Gripper
 from .moveit2_servo import MoveIt2Servo
+from .robot_description import MoveGroupDescription, RobotDescription
+from .session import RobotSession, connect
+
+__all__ = [
+    "robots",
+    "GripperCommand",
+    "GripperInterface",
+    "MoveIt2",
+    "MoveIt2State",
+    "MoveIt2Gripper",
+    "MoveIt2Servo",
+    "MoveGroupDescription",
+    "RobotDescription",
+    "RobotSession",
+    "connect",
+]
